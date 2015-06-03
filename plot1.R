c8a31641ffd374b96fc96b4ee9d69304b3929487
Pawe³ Pacewicz <pawelpacewicz@gmail.com>
2015-06-04 00:25:46 +0200

    final

--- plot1.R
+++ plot1.R
@@ -3,11 +3,10 @@ plot1<-function() {
     # ==============
 
     # read file
-#    temp <- tempfile()
-#    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-#    hpcDF <- read.table(unz(temp, "household_power_consumption.txt"), header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
-#    unlink(temp)
-    hpcDF<-hpcDF_test
+    temp <- tempfile()
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+    hpcDF <- read.table(unz(temp, "household_power_consumption.txt"), header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+    unlink(temp)
 
     # subset dates 2007-02-01 - 2007-02-02
     hpcDF <- hpcDF[hpcDF$Date=="1/2/2007" | hpcDF$Date=="2/2/2007",]
